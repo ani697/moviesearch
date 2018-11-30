75887c9455d96d703891c53118328508627640d3
Aniket Srivastava <aniket697@gmail.com>
2018-11-30 15:40:23 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Links from './Links';
 class App extends Component {
   render() {
     return (
-      <BrowserRouter basename={''}>
+      <BrowserRouter>
         <div className="App">
             <Links />
         </div>
