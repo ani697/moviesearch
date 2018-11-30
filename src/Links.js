71d16653e878c38f91b79e7e6e5ac85558b670ab
Aniket Srivastava <aniket697@gmail.com>
2018-11-30 15:22:18 +0530

    make it better

--- src/Links.js
+++ src/Links.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
 import SearchPage from './SearchPage/SearchPage';
 import FoundPage from './FoundPage/FoundPage';
-import {Route} from 'react-router-dom';
+import {Route,Switch} from 'react-router-dom';
 
 class Links extends Component{
   render(){
     return(
       <div>
-        <Route  path='//' exact  component={SearchPage} />
-        <Route  path='/FoundPage/:Title' component={FoundPage} />
+        <Switch>
+          <Route  path='//' exact  component={SearchPage} />
+          <Route  path='/FoundPage/:Title' component={FoundPage} />
+        </Switch>
       </div>
     );
   }
